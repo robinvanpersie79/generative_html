8e797b95c441501f9d891528e49a0545308f720d
robinvanpersie79 <makineogrenmesibrk@gmail.com>
2023-04-03 02:49:54 +0300

    feat(utils): added textstr support to syntax simplifiers

--- utils/css_utils.py
+++ utils/css_utils.py
@@ -1,5 +1,18 @@
+import os
 import subprocess
 
 
-def css2stylus(filepath):
-    return subprocess.getoutput(f"""stylus --css < {filepath} """)
+def css2stylus(filepath=None, html_text=None):
+    if filepath:
+        return subprocess.getoutput(f"""stylus --css < {filepath} """)
+
+    else:
+        fd, filepath = tempfile.mkstemp(suffix='.txt')
+        try:
+            with os.fdopen(fd, 'w') as tmp:
+                tmp.write(str_)
+
+            out = subprocess.getoutput(f"""stylus --css < {filepath} """)
+        finally:
+            os.remove(filepath)
+        return out
--- utils/html_utils.py
+++ utils/html_utils.py
@@ -1,5 +1,18 @@
+import os
 import subprocess
 
 
-def html2jade(filepath):
-    return subprocess.getoutput(f"""cat {filepath} | html2jade - """)
+def html2jade(filepath=None, html_text=None):
+    if filepath:
+        return subprocess.getoutput(f"""cat {filepath} | html2jade - """)
+
+    else:
+        fd, filepath = tempfile.mkstemp(suffix='.txt')
+        try:
+            with os.fdopen(fd, 'w') as tmp:
+                tmp.write(str_)
+
+            out = subprocess.getoutput(f"""cat {filepath} | html2jade -""")
+        finally:
+            os.remove(filepath)
+        return out
